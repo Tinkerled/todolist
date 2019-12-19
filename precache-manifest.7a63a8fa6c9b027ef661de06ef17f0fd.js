e03b55fcb1b8a3391625d03548091bcd3934178b
Lucas <i.lucas.guillemette@gmail.com>
2019-12-19 17:18:37 -0500

    Updates

---
+++ precache-manifest.7a63a8fa6c9b027ef661de06ef17f0fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "055186b69b69fdf58a1acf8289852f15",
+    "url": "/Tinkerled/todolist/index.html"
+  },
+  {
+    "revision": "391927db461eab34deca",
+    "url": "/Tinkerled/todolist/static/css/main.6cd74b9d.chunk.css"
+  },
+  {
+    "revision": "72f5bd1e0a1386ec63bf",
+    "url": "/Tinkerled/todolist/static/js/2.9d9e6e74.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/Tinkerled/todolist/static/js/2.9d9e6e74.chunk.js.LICENSE"
+  },
+  {
+    "revision": "391927db461eab34deca",
+    "url": "/Tinkerled/todolist/static/js/main.254c8c92.chunk.js"
+  },
+  {
+    "revision": "a3adcbef4f9e5e689363",
+    "url": "/Tinkerled/todolist/static/js/runtime-main.81813e08.js"
+  }
+]);
